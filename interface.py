4a1211147c66fa87835f3382b794b6bcaffb72f8
FengchunFan <ffan005@ucr.edu>
2023-05-25 21:28:20 -0700

    simple search engine to display results in output0.csv

--- interface.py
+++ interface.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__) #instance of flask class
 
 @app.route("/") #root url
 def main():
     return render_template("search.html")
+
+#testings using output0.csv
+@app.route("/output0.csv")
+def retrieve_result():
+    return send_from_directory("static", "output0.csv")
 
 if __name__ == "__main__":
     app.run(debug=True)
