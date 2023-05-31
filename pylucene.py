4f2671db2864c675db46c2aab95f0aeb822d72f2
FengchunFan <ffan005@ucr.edu>
2023-05-31 15:40:11 -0700

    able to import data from sample

---
+++ pylucene.py
@@ -0,0 +1,90 @@
+#used as a backup file for pylucene.py
+#file modified based on the sample provided by Mr.SHIHAB RASHID from CS172 in UC riverside
+
+import logging, sys
+logging.disable(sys.maxsize)
+
+import lucene
+import os
+import csv
+from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType, StringField
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
+from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
+from org.apache.lucene.search.similarities import BM25Similarity
+
+#import the database 
+data_path = './static/sample.csv'
+sample_doc = []
+with open(data_path, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        title = row[0]
+        context = row[1]
+        url = row[2]
+        temp_doc = {
+            'title' : title,
+            'context' : context,
+            'url' : url
+        }
+        sample_doc.append(temp_doc)
+
+def create_index(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    store = SimpleFSDirectory(Paths.get(dir))
+    analyzer = StandardAnalyzer()
+    config = IndexWriterConfig(analyzer)
+    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+    writer = IndexWriter(store, config)
+
+    metaType = FieldType()
+    metaType.setStored(True)
+    metaType.setTokenized(False)
+
+    contextType = FieldType()
+    contextType.setStored(True)
+    contextType.setTokenized(True)
+    contextType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+
+    #retrieve document samples from output.csv files
+    for sample in sample_doc:
+        title = sample['title']
+        context = sample['context']
+        url = sample['url']
+
+        doc = Document()
+        doc.add(Field('Title', str(title), metaType))
+        doc.add(Field('Context', str(context), contextType))
+        doc.add(Field('Url', str(url), StringField.TYPE_STORED))
+        writer.addDocument(doc)
+    writer.close()
+
+def retrieve(storedir, query):
+    searchDir = NIOFSDirectory(Paths.get(storedir))
+    searcher = IndexSearcher(DirectoryReader.open(searchDir))
+    
+    parser = QueryParser('Context', StandardAnalyzer())
+    parsed_query = parser.parse(query)
+
+    topDocs = searcher.search(parsed_query, 10).scoreDocs
+    topkdocs = []
+    for hit in topDocs:
+        doc = searcher.doc(hit.doc)
+        topkdocs.append({
+            "score": hit.score,
+            "title": doc.get("Title"),
+            "text": doc.get("Context"),
+            "url": doc.get("Url")
+        })
+    
+    print(topkdocs)
+
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+create_index('lucene_index/')
+retrieve('lucene_index/', 'riverside')
+
