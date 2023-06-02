4c50cecb8f0fce12f90414120f15d8574bd7ddef
FengchunFan <ffan005@ucr.edu>
2023-06-01 20:58:54 -0700

    reorganized files

--- interface.py
+++ interface.py
@@ -2,15 +2,28 @@
 #export FLASK_APP = interface
 #flask run -h 0.0.0.0 -p 8888
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__) #instance of flask class
 
-@app.route("/") #root url
-def main():
-    return render_template("search.html")
+#@app.route("/") #root url
+#def main():
+#    return render_template("input.html")
 
-#testings using output0.csv
+@app.route('/', methods = ['POST', 'GET'])
+def input():
+    return render_template('input.html')
+
+@app.route('/output', methods = ['POST', 'GET'])
+def output():
+    if request.method == 'GET':
+        return f"Nothing"
+    if request.method == 'POST':
+        form_data = request.form
+        query = form_data['query']
+        print(f"this is the query: {query}")
+        return render_template('output.html')
+
 #@app.route("/output0.csv")
 @app.route("/result.csv")
 def retrieve_result():
